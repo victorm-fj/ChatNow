3fc70752c5bf363bb4bed74c66d9c03ed35db206
victorm-fj <victorm.floresj@gmail.com>
2017-01-17 19:47:38 -0600

    Add Navigator to App component

--- app/App.js
+++ app/App.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  View,
-  Text,
+  Navigator,
 } from 'react-native';
 
 import ChatNow from './components/ChatNow';
 import SignInScreen from './components/SignInScreen';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  sceneContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+  },
+});
+
 class App extends Component {
+  renderScene = (route, navigator) => {
+    switch (route.name) {
+      case 'SignInScreen':
+        return <SignInScreen />;
+      case 'ChatNow':
+      default:
+        return <ChatNow />;
+    }
+  }
+
   render() {
     return (
-      <ChatNow />
+      <Navigator
+        initialRoute={{ name: 'ChatNow', title: 'ChatNow' }}
+        renderScene={this.renderScene}
+        sceneStyle={styles.sceneContainer}
+        style={styles.container}
+      />
     );
   }
 }
